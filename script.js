c998597569cda04d3c29fdc06f34cac01c69a628
BLACKSHOPPOFC <ismaelmagalhaes528@gmail.com>
2026-08-04 00:33:06 -0300

    Add functionality to display letter on button click

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const button = document.getElementById("openLetter");
+const intro = document.getElementById("intro");
+const letter = document.getElementById("letter");
 
+button.addEventListener("click", () => {
+
+    intro.style.display = "none";
+
+    letter.style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
