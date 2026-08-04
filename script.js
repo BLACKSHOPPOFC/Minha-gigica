9a6322ae2636da4e3feb2486ace5cdf91782cf1b
BLACKSHOPPOFC <ismaelmagalhaes528@gmail.com>
2026-08-04 01:30:04 -0300

    Remove unused variables and event listener

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 alert("JavaScript funcionando!");
 
 const button = document.getElementById("openLetter");
-const intro = document.getElementById("intro");
-const letter = document.getElementById("letter");
 
-button.addEventListener("click", () => {
-
-    alert("O botão foi clicado!");
-
-});
-
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-
-});
+alert(button);
